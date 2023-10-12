13d9cb67a7722773456b2362bc42576e572d24f3
M-Eleoterio <m.eleoteriovieira@gmail.com>
2023-10-12 18:15:09 -0300

    v0.1

---
+++ index.js
@@ -0,0 +1,26 @@
+const btnShorter = document.getElementById('btnShorter')
+const btnReload = document.getElementById('btnReload')
+const urlNotShort = document.getElementById('urlNotShortened')
+const urlShort = document.getElementById('urlShortened')
+
+btnShorter.addEventListener('click', shortenURL)
+
+function shortenURL() {
+    let originalURL = urlNotShort.value;
+    let apiURL = "https://tinyurl.com/api-create.php?url=" + encodeURIComponent(originalURL);
+
+    fetch(apiURL).then(response => response.text()).then(data => {
+        urlShort.value = data;
+    }).catch(error => {
+        urlShort.value = "Error: Unable to shortern URL!";
+    })
+
+}
+
+btnReload.addEventListener('click', reloadURL)
+
+function reloadURL() {
+    urlNotShort.value = ""
+    urlShort.value = ""
+}
+
